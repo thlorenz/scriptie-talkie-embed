3bf0889165338aec82a4517829c8668b7756adb9
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-12 23:18:09 -0400

    working editor that gets dynamically loaded

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
-
 /*jshint browser: true*/
+/*global ace */
+
+var loadAce = require('./lib/load-ace');
 
 function harvest(scripties) {
   var res = []
@@ -13,7 +15,32 @@ function harvest(scripties) {
   return res;
 }
 
+function createEditor(replaceElement, id) {
+
+  var container = document.createElement('div');
+  container.setAttribute('id', id);
+  replaceElement.parentNode.replaceChild(container, replaceElement);
+
+  var editor = ace.edit(id);
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/javascript");
+  editor.$highlightActiveLine = false;
+  return editor;
+}
+
+function talkify(scripties) {
+  console.log('talkifying');
+  scripties.forEach(function(scriptie, idx) {
+    var editor = createEditor(scriptie.el, 'scriptie-talkie-ace-editor-' + idx);
+    editor.setValue(scriptie.src);
+  });
+}
+
 module.exports = function () {
-  var scripties = document.getElementsByClassName('scriptie-talkie');
-  window.res = harvest(scripties);
+  var wantScripties = document.getElementsByClassName('scriptie-talkie');
+  var scripties = harvest(wantScripties);
+  if (!scripties.length) return;
+
+  // only load ace editor if we found elements in the page that want to talk
+  loadAce(talkify.bind(null, scripties));
 };
