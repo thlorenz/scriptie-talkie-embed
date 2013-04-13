fe7db3cb207acda3441d8fbb76dd1e324a97865d
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-13 10:50:47 -0400

    working embed without interaction and still some hardcoded values

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 /*jshint browser: true*/
 /*global ace */
 
-var loadAce = require('./lib/load-ace');
+var loadAce = require('./lib/load-ace')
+  , normalizeIndent = require('./lib/normalize-indent')
+  , hypernal = require('hypernal')
+  ;
 
 function harvest(scripties) {
   var res = []
@@ -10,29 +13,61 @@ function harvest(scripties) {
     , el;
   for (var i = 0; i < len; i++) {
     el = scripties.item(i);
-    res.push({ el: el, src: el.textContent });
+    res.push({ el: el, src: normalizeIndent(el.textContent) });
   }
   return res;
 }
 
-function createEditor(replaceElement, id) {
+/*function insertAfter(referenceNode, newNode) {
+  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}*/
 
-  var container = document.createElement('div');
-  container.setAttribute('id', id);
-  replaceElement.parentNode.replaceChild(container, replaceElement);
+function replaceElement(replaceEl, withEl) {
+  replaceEl.parentNode.replaceChild(withEl, replaceEl);
+}
+
+function createEditor(container, src, id) {
+  var editorContainer = document.createElement('div');
+  editorContainer.setAttribute('id', id);
+  editorContainer.setAttribute('class', 'scriptie-talkie-editor');
+  container.appendChild(editorContainer);
 
   var editor = ace.edit(id);
   editor.setTheme("ace/theme/monokai");
   editor.getSession().setMode("ace/mode/javascript");
   editor.$highlightActiveLine = false;
-  return editor;
+  editor.setValue(src);
+}
+
+function createTerminal(container, opts, id) {
+  var terminal = document.createElement('div');
+  terminal.setAttribute('id', id);
+  terminal.setAttribute('class', 'scriptie-talkie-terminal');
+  container.appendChild(terminal);
+
+  var testFontWidth =  20 //document.getElementById('test-font-width').clientWidth + 1
+    , fontSize      =  14 //parseInt(getStyle(terminal, 'font-size'), 10)
+    , fontWidth     =  Math.round(testFontWidth / (26 * 2))
+    , height        =  opts.height
+    , width         =  opts.width 
+    , rows          =  Math.round(height / (fontSize + 6))
+    , cols          =  Math.round(width / fontWidth)
+    ;
+
+  //var term = hypernal(cols, rows);
+  var term = hypernal(60, 40);
+  term.appendTo(terminal);
+  term.writeln('hello');
+  return term;
 }
 
 function talkify(scripties) {
-  console.log('talkifying');
   scripties.forEach(function(scriptie, idx) {
-    var editor = createEditor(scriptie.el, 'scriptie-talkie-ace-editor-' + idx);
-    editor.setValue(scriptie.src);
+    var container = document.createElement('div');
+    replaceElement(scriptie.el, container);
+
+    var editor = createEditor(container, scriptie.src, 'scriptie-talkie-ace-editor-' + idx);
+    var terminal = createTerminal(container, { height: 500, width: 500 }, 'scriptie-talkie-terminal-' + idx);
   });
 }
 
