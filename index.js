804c7f432aaa9cb9590a0e2ecea9c6c9c5712302
Thorsten Lorenz <thlorenz@gmx.de>
2013-04-13 16:10:08 -0400

    separating creation of elements

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
-/*jshint browser: true*/
-/*global ace */
 
-var loadAce = require('./lib/load-ace')
-  , normalizeIndent = require('./lib/normalize-indent')
-  , hypernal = require('hypernal')
+var loadAce         =  require('./lib/load-ace')
+  , normalizeIndent =  require('./lib/normalize-indent')
+  , debounce        =  require('debounce')
+  , createContainer =  require('./lib/create-container')
+  , createEditor    =  require('./lib/create-editor')
+  , createTerminal  =  require('./lib/create-terminal')
+  , evaluateScript  =  require('./lib/evaluate-script')
   ;
 
 function harvest(scripties) {
@@ -18,56 +20,25 @@ function harvest(scripties) {
   return res;
 }
 
-/*function insertAfter(referenceNode, newNode) {
-  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}*/
-
-function replaceElement(replaceEl, withEl) {
-  replaceEl.parentNode.replaceChild(withEl, replaceEl);
-}
-
-function createEditor(container, src, id) {
-  var editorContainer = document.createElement('div');
-  editorContainer.setAttribute('id', id);
-  editorContainer.setAttribute('class', 'scriptie-talkie-editor');
-  container.appendChild(editorContainer);
-
-  var editor = ace.edit(id);
-  editor.setTheme("ace/theme/monokai");
-  editor.getSession().setMode("ace/mode/javascript");
-  editor.$highlightActiveLine = false;
-  editor.setValue(src);
-}
-
-function createTerminal(container, opts, id) {
-  var terminal = document.createElement('div');
-  terminal.setAttribute('id', id);
-  terminal.setAttribute('class', 'scriptie-talkie-terminal');
-  container.appendChild(terminal);
-
-  var testFontWidth =  20 //document.getElementById('test-font-width').clientWidth + 1
-    , fontSize      =  14 //parseInt(getStyle(terminal, 'font-size'), 10)
-    , fontWidth     =  Math.round(testFontWidth / (26 * 2))
-    , height        =  opts.height
-    , width         =  opts.width 
-    , rows          =  Math.round(height / (fontSize + 6))
-    , cols          =  Math.round(width / fontWidth)
-    ;
-
-  //var term = hypernal(cols, rows);
-  var term = hypernal(60, 40);
-  term.appendTo(terminal);
-  term.writeln('hello');
-  return term;
-}
 
 function talkify(scripties) {
   scripties.forEach(function(scriptie, idx) {
-    var container = document.createElement('div');
-    replaceElement(scriptie.el, container);
 
-    var editor = createEditor(container, scriptie.src, 'scriptie-talkie-ace-editor-' + idx);
-    var terminal = createTerminal(container, { height: 500, width: 500 }, 'scriptie-talkie-terminal-' + idx);
+    var container         =  createContainer(scriptie.el);
+    var term              =  createTerminal(container, 'scriptie-talkie-terminal-' + idx)
+      , terminal          =  term.terminal
+      , terminalContainer =  term.container;
+    var edit              =  createEditor(container, scriptie.src, 'scriptie-talkie-ace-editor-' + idx)
+      , editor            =  edit.editor
+      , editorContainer   =  edit.container;
+
+    function evaluate() {
+      terminal.reset();
+      evaluateScript(editor.getValue(), terminal.writeln.bind(terminal));
+    }
+
+    editor.on('change', debounce(evaluate, 400, false));
+    evaluate();
   });
 }
 
